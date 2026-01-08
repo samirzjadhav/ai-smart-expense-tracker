3675687b618c1bbba81c85f8700ced9ac7aaee43
samirzjadhav <samirzjadhav@gmail.com>
2026-01-08 10:30:00 +0000

    feat: change layout and page seciton

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h2>hello</h2>;
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold mb-4">AI Smart Expense Tracker</h1>
+      <p className="text-gray-600 mb-6">
+        Track your expenses and get AI-powered saving tips
+      </p>
+
+      <Link
+        href="/dashboard"
+        className="bg-blue-600 text-white px-6 py-3 rounded"
+      >
+        Go to Dashboard
+      </Link>
+    </div>
+  );
 }
